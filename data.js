24d0ac5cf5f91ab787bfa3032e4b9dd968241e89
Alex Preager <apreager@gmail.com>
2026-02-03 11:31:35 +0000

    Initial pano upload

---
+++ data.js
@@ -0,0 +1,245 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-tea-auction---reception_1",
+      "name": "Tea Auction - Reception_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6230968966536494,
+          "pitch": 0.08831493461786799,
+          "rotation": 0,
+          "target": "1-tea-auction---open-plan_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-tea-auction---open-plan_1",
+      "name": "Tea Auction - Open Plan_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.89575247161458,
+          "pitch": 0.005185035743238586,
+          "rotation": 6.283185307179586,
+          "target": "0-tea-auction---reception_1"
+        },
+        {
+          "yaw": -0.3207042402985856,
+          "pitch": 0.21229525246076086,
+          "rotation": 0,
+          "target": "2-tea-auction---open-plan-creative_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-tea-auction---open-plan-creative_1",
+      "name": "Tea Auction - Open Plan Creative_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3344104826258061,
+          "pitch": 0.15737202232388725,
+          "rotation": 0,
+          "target": "3-tea-auction---cellular_1"
+        },
+        {
+          "yaw": -2.8920569696368226,
+          "pitch": 0.02754693417575993,
+          "rotation": 0,
+          "target": "0-tea-auction---reception_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-tea-auction---cellular_1",
+      "name": "Tea Auction - Cellular_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.35055793390899836,
+          "pitch": 0.03358568143044138,
+          "rotation": 0,
+          "target": "4-tea-auction---1---cat-a_1"
+        },
+        {
+          "yaw": -2.898903267175953,
+          "pitch": 0.018995405604211868,
+          "rotation": 0,
+          "target": "0-tea-auction---reception_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-tea-auction---1---cat-a_1",
+      "name": "Tea Auction - 1 - Cat A_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.31932526297069685,
+          "pitch": 0.04159186859309649,
+          "rotation": 0,
+          "target": "1-tea-auction---open-plan_1"
+        },
+        {
+          "yaw": -2.8765857585084866,
+          "pitch": 0.02654010113349692,
+          "rotation": 0,
+          "target": "0-tea-auction---reception_1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Panotour Example",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
